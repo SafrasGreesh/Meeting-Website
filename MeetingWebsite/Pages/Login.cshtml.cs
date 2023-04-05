1708c38709ab6daf0fd5e214838f8fbc9595c5ab
Nikita <tohdhjyh4698@gmail.com>
2023-04-05 21:42:05 +0400

    сверстали окна авторизации и регистрации

---
+++ MeetingWebsite/Pages/Login.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeetingWebsite.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MeetingWebsite/Pages/Registration.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeetingWebsite.Pages
+{
+    public class RegistrationModel : PageModel
+    {
+        private readonly ILogger<RegistrationModel> _logger;
+
+        public RegistrationModel(ILogger<RegistrationModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
