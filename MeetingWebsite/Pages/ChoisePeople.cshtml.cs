b94a32988d590463ff7e5c811e5c95dfdbfcfd99
Nikita <tohdhjyh4698@gmail.com>
2023-04-20 23:41:30 +0400

    добавил выбор анкет

---
+++ MeetingWebsite/Pages/ChoisePeople.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeetingWebsite.Pages
+{
+    public class ChoisePeopleModel : PageModel
+    {
+        private readonly ILogger<ChoisePeopleModel> _logger;
+
+        public ChoisePeopleModel(ILogger<ChoisePeopleModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
