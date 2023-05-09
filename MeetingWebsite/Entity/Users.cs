594a02fcbe9ad4d0a1f00dffcaf1817a28adf387
financenick <mnick.direct@gmail.com>
2023-05-09 15:05:29 +0400

    sdf

--- MeetingWebsite/Entity/Users.cs
+++ MeetingWebsite/Entity/Users.cs
@@ -4,7 +4,7 @@ namespace MeetingWebsite.Entity
 {
     public class Users
     {
-        private int Password { get; set; }
+        public int Password { get; set; }
 
         public int? Id { get; set; }
         public string Name { get; set; }
