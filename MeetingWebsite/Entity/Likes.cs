7971a39e6d5e909dafc15963709609d8c6159edd
Nikita <tohdhjyh4698@gmail.com>
2023-05-21 03:14:04 +0400

    Squashed commit of the following:
    commit 5e4171214606625e6efd217c0e441a7457f7b295
    Author: Nikita <tohdhjyh4698@gmail.com>
    Date:   Sat May 20 20:37:02 2023 +0400
    
        доработка свайпов
    
        добавил взаимные лайки, настроил отображение анкет по параметрам, убрал отображение уже оцененных анкет
    
    commit ec37c5d9760806a50fccdec329eb135560de7137
    Author: Nikita <tohdhjyh4698@gmail.com>
    Date:   Sat May 20 03:09:15 2023 +0400
    
        Параметры поиска
    
        Добавил таблицу параметров, сделал запись в бд со страницы, начал настройку отображения из бд
    
    commit c83e4ab1ebc3aa9b641109fd8145dcd17c682759
    Author: Nikita <tohdhjyh4698@gmail.com>
    Date:   Fri May 19 03:00:45 2023 +0400
    
        добавил подгрузку данных из бд при свайпе анкет

--- MeetingWebsite/Entity/Likes.cs
+++ MeetingWebsite/Entity/Likes.cs
@@ -1,12 +1,12 @@
 ﻿namespace MeetingWebsite.Entity
 {
-    public class Likes
+    public class Likes : BaseEntity
     {
         public int? Id { get; set; }
             
         public int? UserId { get; set; }
         public int? LikeUserId { get; set; }
         public DateTime CreatedAt { get; set; }
-        public Users Users { get; set; } = null!;
+        public Boolean Like { get; set; }
     }
 }
